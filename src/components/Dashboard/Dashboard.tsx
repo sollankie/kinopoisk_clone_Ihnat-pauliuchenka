0ed70d0089fccd92c0531e161a13996f7aa598e9
sollankie <ignat.pauliuchenka@yandex.ru>
2023-11-26 12:46:22 +0300

    added dashboard

---
+++ src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <h2>Dashboard234</h2>
+      {/* Здесь можете добавить содержимое вашей страницы Dashboard */}
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from '../components/Home';
 import About from '../components/About';
-// import Dashboard from '../components/Dashboard';
+import Dashboard from '../components/Dashboard/Dashboard';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
