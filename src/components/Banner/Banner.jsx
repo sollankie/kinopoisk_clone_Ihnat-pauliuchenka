a25a4a594e1521235460978dc0f298316e00a42d
sollankie <ignat.pauliuchenka@yandex.ru>
2023-11-29 10:14:40 +0300

    added API, Banner with API Using

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,33 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Banner.module.scss'; 
+import axios from '../API/axios';
+import requests from '../API/Requests';
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
 
-    function truncate(string, n) {
-        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+
+        if (request.data.results && request.data.results.length > 0) {
+          setMovie(
+            request.data.results[
+              Math.floor(Math.random() * request.data.results.length)
+            ]
+          );
+        } else {
+          console.error('Ошибка: пустой ответ или отсутствуют результаты');
+        }
+      } catch (error) {
+        console.error('Ошибка при выполнении запроса:', error);
+      }
     }
+
+    fetchData();
+  }, []);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+  }
+
+  
+  const backdropPath = movie?.backdrop_path;
+
+  
+  const imageUrl = backdropPath
+    ? `https://image.tmdb.org/t/p/original/${backdropPath}`
+    : ''; 
 
   return (
     <header
       className={styles.banner} 
       style={{
-        backgroundImage: `url('https://marcos.kirsch.mx/wp-content/uploads/2013/01/cropped-cropped-cropped-black-banner.png')`,
+        backgroundImage: `url('${imageUrl}')`,
         backgroundSize: 'cover',
         backgroundPosition: 'center center',
       }}
     >
-        <div className={styles.banner_content}>
-            <h1 className={styles.banner_title}>Movie Name</h1>
-            <div className={styles.banner_buttons}>
-                <button className={styles.banner_button}>Play</button>
-                <button className={styles.banner_button}>My List</button>
-            </div>
-            <h1 className={styles.banner_description}>
-                {truncate(`Test descxirption`, 150)}
-            </h1>
+      <div className={styles.banner_content}>
+        <h1 className={styles.banner_title}>Movie Name</h1>
+        <div className={styles.banner_buttons}>
+          <button className={styles.banner_button}>Play</button>
+          <button className={styles.banner_button}>My List</button>
         </div>
+        <h1 className={styles.banner_description}>
+          {truncate(`Test description`, 150)}
+        </h1>
+      </div>
 
-        <div className={styles.banner_fadeBottom} />
+      <div className={styles.banner_fadeBottom} />
     </header>
   );
 }
