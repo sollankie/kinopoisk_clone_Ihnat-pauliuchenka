100c40d01dddff6b170f2464be9b359431ab4380
sollankie <ignat.pauliuchenka@yandex.ru>
2023-12-13 21:33:17 +0300

    added firebase

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAvlat4PVHZpKgobDHnNHY_qvRiYPGSsX0",
+    authDomain: "kinopoisk-clone-8683c.firebaseapp.com",
+    projectId: "kinopoisk-clone-8683c",
+    storageBucket: "kinopoisk-clone-8683c.appspot.com",
+    messagingSenderId: "675639438229",
+    appId: "1:675639438229:web:28c0124203d9b7dad4a49c"
+  };
