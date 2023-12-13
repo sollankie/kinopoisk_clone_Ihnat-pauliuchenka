95c9e78a1e512ffed3f2d25b79dfdcca06cdd4bc
sollankie <ignat.pauliuchenka@yandex.ru>
2023-12-13 21:34:24 +0300

    added routing for login

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './components/Screens/HomeScreen/HomeScreen';
 import MovieScreen from './components/Screens/MovieScreen/MovieScreen';
 import './App.scss';
+import LoginScreen from './components/Screens/LoginScreen/LoginScreen';
 
 function App() {
+    const user = null;
+
+
     return (
         <Router>
+            {!user ? (
+                <LoginScreen />
+            ) : (
             <Routes>
                 <Route path="/" element={<HomeScreen />} />
                 <Route path="/movie/:movieId" element={<MovieScreen />} />
+                
             </Routes>
+            )}
         </Router>
     );
 }
