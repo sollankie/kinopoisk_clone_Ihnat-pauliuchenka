c9771023598eda7f71cb12a47bd324fa82a321be
sollankie <ignat.pauliuchenka@yandex.ru>
2023-12-18 23:04:52 +0300

    update ProfileScreen

--- src/components/Screens/ProfileScreen/ProfileScreen.jsx
+++ src/components/Screens/ProfileScreen/ProfileScreen.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import Nav from '../../Nav/Nav.tsx';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../../features/userSlice';
+import { auth } from '../../Firebase/firebase.js';
 import styles from './ProfileScreen.module.scss';
 
-function ProfileScreen() {
-  return (
-    <div>ProfileScreen</div>
-  )
-}
+const ProfileScreen = () => {
+  const user = useSelector(selectUser);
 
-export default ProfileScreen
+  return (
+    <div className={styles.profileScreen}>
+      <Nav />
+      <div className={styles.profileScreen_body}>
+        <h1>Edit Profile</h1>
+        <div className={styles.profileScreen_info}>
+          <img
+            src="https://artscimedia.case.edu/wp-content/uploads/sites/79/2016/12/14205134/no-user-image.gif"
+            alt=""
+          />
+          <div className={styles.profileScreen_details}>
+            <h2>{user.email}</h2>
+          
+          <div className={styles.profileScreen_favorites}>
+            <h3>Your Favorites</h3>
+            {/* FAVORITES FILMS ROW */}
+            <button onClick={() => auth.signOut()} 
+            className={styles.profileScreen_signOut}>
+              Sign Out
+            </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileScreen;
